d2e556d6c97a9687c9b87ef867106063a1e0a841
rhit-ciminodj <ciminodj@rose-hulman.edu>
2026-05-04 23:58:20 -0400

    Basic character movement and shooting, started on enemies

--- HaskellVideoGame/app/Main.hs
+++ HaskellVideoGame/app/Main.hs
@@ -17,21 +17,38 @@ background = black
 data KaelinGame = Game
     { kaelinLoc :: (Float, Float),
             kaelinVelX :: Float,
-            kaelinVelY :: Float
+            kaelinVelY :: Float,
+        spawnHeld :: Bool,
+        spawnTimer :: Float,
+        friendlyProjectiles :: [(Float, Float)],
+        projectileVelocity :: Float,
+        enemies :: [(Float, Float)],
+        enemySpeed :: Float,
+        enemyProjectiles :: [(Float, Float)]
     } deriving Show
 
 render :: KaelinGame -> Picture
-render game = 
-    pictures [kaelin]
+render game =
+        pictures (kaelin : (enemyPics ++ projectilePics ++ enemyProjectilePics))
     where
         kaelin = color white $ uncurry translate (kaelinLoc game) $ circleSolid 10
+        enemyPics = map (\t -> uncurry translate t $ color orange $ circleSolid 10) (enemies game)
+        projectilePics = map (\t -> uncurry translate t $ color green $ circleSolid 3) (friendlyProjectiles game)
+        enemyProjectilePics = map (\t -> uncurry translate t $ color red $ circleSolid 3) (enemyProjectiles game)
 
 
 initialState :: KaelinGame
 initialState = Game
     { kaelinLoc = (0, -200),
             kaelinVelX = 0,
-            kaelinVelY = 0
+            kaelinVelY = 0,
+            spawnHeld = False,
+            spawnTimer = 0,
+        friendlyProjectiles = [],
+        projectileVelocity = 300,
+        enemies = [],
+        enemySpeed = 100,
+        enemyProjectiles = []
     }
 
 moveKaelin :: Float -> KaelinGame -> KaelinGame
@@ -43,6 +60,20 @@ moveKaelin seconds game = game { kaelinLoc = (x', y') }
 
         x' = x + vx * seconds
         y' = y + vy * seconds
+
+moveProjectile :: Float -> KaelinGame ->  KaelinGame
+moveProjectile seconds game = game { friendlyProjectiles = filter inBounds moved }
+    where
+        moved = map moveOne (friendlyProjectiles game)
+        moveOne (x, y) = (x, y + projectileVelocity game * seconds)
+        inBounds (_, y) = y >= (-fromIntegral height / 2) && y <= fromIntegral height / 2
+
+moveEnemyProjectile :: Float -> KaelinGame -> KaelinGame
+moveEnemyProjectile seconds game = game { enemyProjectiles = filter inBounds moved }
+    where
+        moved = map moveOne (enemyProjectiles game)
+        moveOne (x, y) = (x, y - projectileVelocity game * seconds)
+        inBounds (_, y) = y >= (-fromIntegral height / 2) && y <= fromIntegral height / 2
 
 fps :: Int
 fps = 60
@@ -67,6 +98,12 @@ handleKeys (EventKey (Char 'd') Down _ _) game =
 handleKeys (EventKey (Char 'w') Up _ _) game =
     game { kaelinVelY = 0}
 
+handleKeys (EventKey (SpecialKey KeySpace) Down _ _) game =
+    game { spawnHeld = True }
+
+handleKeys (EventKey (SpecialKey KeySpace) Up _ _) game =
+    game { spawnHeld = False }
+
 handleKeys (EventKey (Char 's') Up _ _) game =
     game { kaelinVelY = 0}
 
@@ -76,10 +113,19 @@ handleKeys (EventKey (Char 'a') Up _ _) game =
 handleKeys (EventKey (Char 'd') Up _ _) game =
     game { kaelinVelX = 0}
 
+
+
 handleKeys _ game = game
 
 main :: IO ()
 main = play window background fps initialState render handleKeys update
 
 update :: Float -> KaelinGame -> KaelinGame
-update seconds game = moveKaelin seconds game
+update seconds game =
+    let moved = moveProjectile seconds (moveKaelin seconds game)
+        timer = spawnTimer moved - seconds
+        cooldown = 0.5
+    in if spawnHeld moved && timer <= 0
+       then moved { friendlyProjectiles = kaelinLoc moved : friendlyProjectiles moved
+                  , spawnTimer = cooldown }
+       else moved { spawnTimer = max 0 timer }
