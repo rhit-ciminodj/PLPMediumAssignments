d382b74bca6c4d126fe2a08abea046ee70445edf
rhit-ciminodj <ciminodj@rose-hulman.edu>
2026-05-04 20:25:36 -0400

    Movement

--- HaskellVideoGame/app/Main.hs
+++ HaskellVideoGame/app/Main.hs
@@ -16,27 +16,30 @@ background = black
 
 data KaelinGame = Game
     { kaelinLoc :: (Float, Float),
-      kaelinVel :: (Float, Float),
-      } deriving Show
+            kaelinVelX :: Float,
+            kaelinVelY :: Float
+    } deriving Show
 
 render :: KaelinGame -> Picture
 render game = 
     pictures [kaelin]
     where
-        kaelin = uncurry translate (kaelinLoc game) $ circleSolid 10
+        kaelin = color white $ uncurry translate (kaelinLoc game) $ circleSolid 10
 
 
-initialState :: PongGame
+initialState :: KaelinGame
 initialState = Game
-    { ballLoc = (0, 200),
-      ballVel = (0, 0),
+    { kaelinLoc = (0, -200),
+            kaelinVelX = 0,
+            kaelinVelY = 0
     }
 
-moveBall :: Float -> PongGame -> PongGame
-moveBall seconds game = game { ballLoc = (x', y') }
+moveKaelin :: Float -> KaelinGame -> KaelinGame
+moveKaelin seconds game = game { kaelinLoc = (x', y') }
     where
-        (x, y) = ballLoc game
-        (vx, vy) = ballVel game
+        (x, y) = kaelinLoc game
+        vx = kaelinVelX game
+        vy = kaelinVelY game
 
         x' = x + vx * seconds
         y' = y + vy * seconds
@@ -44,75 +47,39 @@ moveBall seconds game = game { ballLoc = (x', y') }
 fps :: Int
 fps = 60
 
-wallBounce :: PongGame -> PongGame
-paddleBounce :: PongGame -> PongGame
-
-type Radius = Float
-type Position = (Float, Float)
-
-wallCollision :: Position -> Radius -> Bool
-wallCollision (_, y) radius = topCollision || bottomCollision
-    where
-        topBoundary = fromIntegral height / 2
-        bottomBoundary = negate topBoundary
-
-        topCollision = y + radius >= topBoundary
-        bottomCollision = y - radius <= bottomBoundary
-
-wallBounce game = game { ballVel = (vx, vy') }
-    where
-        radius = 10
-        (vx, vy) = ballVel game
-        
-        vy' = if wallCollision (ballLoc game) radius
-            then
-                -vy
-            else
-                vy
-
-paddleBounce game
-    | hitPaddle = game { ballVel = (-vx, vy) }
-    | otherwise  = game
-  where
-    radius = 10
-    (vx, vy) = ballVel game
-    (x, y) = ballLoc game
-
-    paddleX = 120
-    paddleHalfWidth = 13
-    paddleHalfHeight = 43
-
-    hitPaddle =
-      (abs (x - paddleX) <= paddleHalfWidth + radius && abs (y - player1 game) <= paddleHalfHeight + radius) ||
-      (abs (x + paddleX) <= paddleHalfWidth + radius && abs (y - player2 game) <= paddleHalfHeight + radius)
-      
-paddleSpeed :: Float
-paddleSpeed = 20
-
-handleKeys :: Event -> PongGame -> PongGame
+handleKeys :: Event -> KaelinGame -> KaelinGame
 
 handleKeys (EventKey (Char 'r') _ _ _) game =
-    game { ballLoc = (0, 0) }
-
-handleKeys (EventKey (Char 'p') Down _ _) game = 
-    game { paused = not (paused game) }
+    game { kaelinLoc = (0, 0) }
 
 handleKeys (EventKey (Char 'w') Down _ _) game =
-    game { player2 = (player2 game) + paddleSpeed }
+    game { kaelinVelY = 250}
 
 handleKeys (EventKey (Char 's') Down _ _) game =
-    game { player2 = (player2 game) - paddleSpeed }
+    game { kaelinVelY = -250}
 
-handleKeys (EventKey (SpecialKey KeyUp) Down _ _) game =
-    game { player1 = (player1 game) + paddleSpeed }
+handleKeys (EventKey (Char 'a') Down _ _) game =
+    game { kaelinVelX = -250}
 
-handleKeys (EventKey (SpecialKey KeyDown) Down _ _) game =
-    game { player1 = (player1 game) - paddleSpeed }
+handleKeys (EventKey (Char 'd') Down _ _) game =
+    game { kaelinVelX = 250}
+
+handleKeys (EventKey (Char 'w') Up _ _) game =
+    game { kaelinVelY = 0}
+
+handleKeys (EventKey (Char 's') Up _ _) game =
+    game { kaelinVelY = 0}
+
+handleKeys (EventKey (Char 'a') Up _ _) game =
+    game { kaelinVelX = 0}
+
+handleKeys (EventKey (Char 'd') Up _ _) game =
+    game { kaelinVelX = 0}
 
 handleKeys _ game = game
 
 main :: IO ()
 main = play window background fps initialState render handleKeys update
 
-update :: Float -> PongGame -> PongGame
-update seconds game = if paused game then game else paddleBounce $ wallBounce $ moveBall seconds game
+update :: Float -> KaelinGame -> KaelinGame
+update seconds game = moveKaelin seconds game
